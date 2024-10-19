d27a0085d42c09465b959fa2d9953f59c2ab04b0
Sakib <rabiulahmedsakib@gmail.com>
2024-10-19 21:46:54 +0600

    implement a final class having all API endpoints.

---
+++ src/main/java/com/example/javaproductparser/parser/controller/ApiEndpoints.java
@@ -0,0 +1,9 @@
+package com.example.javaproductparser.parser.controller;
+
+public final class ApiEndpoints {
+    public static final String BASE = "/api/product";
+    public static final String UPLOAD = "/upload";
+    public static final String CHANGE_HISTORIES = "/change-histories";
+    public static final String PRODUCT_BY_SKU = "/product-by-sku";
+    public static final String PRODUCT_ALL = "/product/all";
+}
