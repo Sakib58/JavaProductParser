aeeebc4b2873ca1535132d22d00b5afc3d5fa906
Sakib <rabiulahmedsakib@gmail.com>
2024-10-19 16:03:28 +0600

    Initial setup completed

---
+++ src/main/java/com/example/javaproductparser/JavaProductParserApplication.java
@@ -0,0 +1,13 @@
+package com.example.javaproductparser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaProductParserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaProductParserApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/javaproductparser/JavaProductParserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javaproductparser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaProductParserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
