16296c358895b6443b2ca4ebb02622cdf611bb4c
Sakib <rabiulahmedsakib@gmail.com>
2024-10-20 20:24:39 +0600

    changed API endpoint for getting product list.

--- src/main/java/com/example/javaproductparser/parser/controller/ApiEndpoints.java
+++ src/main/java/com/example/javaproductparser/parser/controller/ApiEndpoints.java
@@ -5,5 +5,5 @@ public final class ApiEndpoints {
     public static final String UPLOAD = "/upload";
     public static final String CHANGE_HISTORIES = "/change-histories";
     public static final String PRODUCT_BY_SKU = "/product-by-sku";
-    public static final String PRODUCT_ALL = "/product/all";
+    public static final String PRODUCT_ALL = "/all";
 }
