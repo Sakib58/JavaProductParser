f8d0b17ce1e5e5aa31fd55f5e9c803b4c7578acd
Sakib <rabiulahmedsakib@gmail.com>
2024-10-20 09:31:53 +0600

    added API to get product info by SKU.

--- src/main/java/com/example/javaproductparser/parser/controller/ProductRestController.java
+++ src/main/java/com/example/javaproductparser/parser/controller/ProductRestController.java
@@ -1,6 +1,7 @@
 package com.example.javaproductparser.parser.controller;
 
 import com.example.javaproductparser.parser.dto.ProductChangeSummaryDto;
+import com.example.javaproductparser.parser.dto.ProductDto;
 import com.example.javaproductparser.parser.entity.ProductChangeSummary;
 import com.example.javaproductparser.parser.service.ProductService;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +11,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @RestController
 @RequestMapping(ApiEndpoints.BASE)
@@ -44,6 +46,18 @@ public class ProductRestController {
         try{
             List<ProductChangeSummary> productChangeSummaries = productService.getSummaryHistoryOfTheUploadedFile();
             return ResponseEntity.ok(productChangeSummaries);
+        } catch (Exception e) {
+            return ResponseEntity.internalServerError().build();
+        }
+    }
+
+    @GetMapping(ApiEndpoints.PRODUCT_BY_SKU)
+    public ResponseEntity<ProductDto> getProductInfoBySku(@RequestParam("sku") String sku) {
+        try {
+            ProductDto productDto = productService.getProductInfoBySku(sku);
+            return ResponseEntity.ok(productDto);
+        } catch (NoSuchElementException noSuchElementException) {
+            return ResponseEntity.notFound().build();
         } catch (Exception e) {
             return ResponseEntity.internalServerError().build();
         }
